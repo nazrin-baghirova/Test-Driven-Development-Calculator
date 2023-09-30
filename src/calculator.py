991b51cf6fea50875c595ee7c11811d24c0fd160
nazrin-baghirova <63057970+nazrin-baghirova@users.noreply.github.com>
2023-09-30 17:34:07 +0000

    Added test suit for multiplier functionality

--- src/calculator.py
+++ src/calculator.py
@@ -9,3 +9,7 @@ def add(num1, num2):
 @check_supported_operand_types
 def subtract(num1, num2):
   return num1 - num2
+
+@check_supported_operand_types
+def multiply(num1, num2):
+  return num1*num2
---
+++ test/test_multiply.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.calculator import multiply
+
+
+def test_multiply_two_integers():
+  assert multiply(2, 2) == 4
+
+
+def test_multiply_int_and_float():
+  assert multiply(2, 3.5) == 7
+
+
+def test_multiply_float_and_float():
+  assert multiply(2.0, 5.0) == 10.0
+
+
+def test_multiply_float_and_zero():
+  assert multiply(2.2, 0) == 0
+
+
+def test_multiply_int_and_list():
+  with pytest.raises(TypeError):
+    assert multiply(19, [])
+
+
+def test_multiply_neg_int_and_float():
+  assert multiply(-1, 5.5) == -5.5
